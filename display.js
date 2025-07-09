9041497a33bc4c83d3e43730b6cad050a16ed184
Sapien2828 <simaumanobanbi@gmail.com>
2025-07-10 07:16:53 +0900

    Add files via upload

--- display.js
+++ display.js
@@ -45,7 +45,7 @@ window.addEventListener('DOMContentLoaded', () => {
           if (waitCount > 0) {
               const earliestPatient = patientsForThisGroup.reduce((earliest, current) => new Date(earliest.receptionTime) < new Date(current.receptionTime) ? earliest : current);
               if (earliestPatient && earliestPatient.receptionTime) {
-                  waitTime = Math.round((new Date() - earliestPatient.receptionTime) / (1000 * 60));
+                  waitTime = Math.round((new Date() - new Date(earliestPatient.receptionTime)) / (1000 * 60));
               }
           }
           const roomNameShort = groupName.split('(')[0];
@@ -106,13 +106,6 @@ window.addEventListener('DOMContentLoaded', () => {
                   container.classList.remove('dense-view');
               }
           });
-      }
-
-      const isIos = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-      const iosPrompt = document.getElementById('ios-prompt');
-      if (isIos && iosPrompt) {
-          iosPrompt.innerHTML = '<p><b>【iPhone/iPadをお使いの方へ】</b><br>全画面で表示するには、下の共有アイコン ( <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/></svg> ) をタップし、「ホーム画面に追加」を選択してください。</p>';
-          iosPrompt.style.display = 'block';
       }
   }
 
